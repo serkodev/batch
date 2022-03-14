548482c3b16bc8d001f7238aac0805f3033cd33e
SerKo <serko.dev@gmail.com>
2022-03-14 23:51:40 +0800

    add benchmarks compare

--- BENCHMARKS.md
+++ BENCHMARKS.md
@@ -2,3 +2,19 @@
 
 ## BenchmarkBatch
 `BenchmarkBatch-12 2469973 441.0 ns/op 231 B/op 3 allocs/op`
+
+## BenchmarkBatchDataLoader
+```
+2022/03/14 23:48:56 avg: 0.000000
+2022/03/14 23:48:56 avg: 28377.200000
+2022/03/14 23:48:57 avg: 29822.363636
+ 2299012	       522.8 ns/op	     244 B/op	       4 allocs/op
+```
+
+Compare [dataloader](https://github.com/graph-gophers/dataloader)
+```
+2022/03/14 23:35:43 avg: 0.000000
+2022/03/14 23:35:44 avg: 20093.500000
+2022/03/14 23:35:45 avg: 22113.536364
+ 1435984	       738.4 ns/op	     492 B/op	       8 allocs/op
+```
--- benchmarks_test.go
+++ benchmarks_test.go
@@ -1,6 +1,12 @@
 package batch
 
-import "testing"
+import (
+	"log"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+)
 
 func BenchmarkBatch(b *testing.B) {
 	a, _ := New(func(ids TaskList[int, string]) {
@@ -10,4 +16,43 @@ func BenchmarkBatch(b *testing.B) {
 		a.Task(i)
 	}
 	a.Task(b.N).Wait()
+}
+
+// see: https://github.com/graph-gophers/dataloader/blob/master/dataloader_test.go
+
+var avg = &Avg{}
+
+func BenchmarkBatchDataLoader(b *testing.B) {
+	a, _ := New(func(ids TaskList[string, interface{}]) {
+		avg.Add(len(ids))
+		ids.Return(nil, nil)
+	}, 16*time.Millisecond, 0).Run()
+	for i := 0; i < b.N; i++ {
+		a.Task(strconv.Itoa(i))
+	}
+	log.Printf("avg: %f", avg.Avg())
+}
+
+type Avg struct {
+	total  float64
+	length float64
+	lock   sync.RWMutex
+}
+
+func (a *Avg) Add(v int) {
+	a.lock.Lock()
+	a.total += float64(v)
+	a.length++
+	a.lock.Unlock()
+}
+
+func (a *Avg) Avg() float64 {
+	a.lock.RLock()
+	defer a.lock.RUnlock()
+	if a.total == 0 {
+		return 0
+	} else if a.length == 0 {
+		return 0
+	}
+	return a.total / a.length
 }
